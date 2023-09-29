60534b751a1eb23496861171d277fff8cd22407c
DudekMarek <dudek371@gmail.com>
2023-09-29 20:57:45 +0200

    initial commit

---
+++ src/database_connection.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+import os
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+connection_string = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = sqlalchemy.create_engine(connection_string)
+
+connection = engine.connect()
+connection.close()
